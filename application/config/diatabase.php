7ce028943aa6c2f0aad15e9227db55fb94e865f4
joseguru <joskip5@gmail.com>
2014-11-25 20:36:01 +0300

    rename diatabase to database.php

--- application/config/diatabase.php
+++ application/config/diatabase.php
@@ -62,8 +62,8 @@ $db['default']['username'] = 'ocsdnet_apply';
 $db['default']['password'] = 'OCSDNet2014';
  */
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'geranimo';
+$db['default']['username'] = 'ocsdnet_proposal';
+$db['default']['password'] = 'OCSDNet2014';
 $db['default']['database'] = 'ocsdnet_staging';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
