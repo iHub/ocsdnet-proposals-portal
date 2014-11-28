2d31decd728240f7fb08e5f613cbe23246acbbe8
JayJay <jjlangatjose@gmail.com>
2014-11-29 01:02:22 +0300

    Edited the edit collaborator view

--- application/views/users/edit_user.php
+++ application/views/users/edit_user.php
@@ -46,3 +46,117 @@
           </div>
         </div>
     </header>
+<div class="container">
+    <div class="">
+        <div class="col-md-12">
+        <form  class="collaborator-info" action="">
+            <fieldset>
+              <h3 class="fs-subtitle">Edit Collaborators</h3>
+              <div class="col-md-6">
+                <input type="hidden" id="researcher_id" name="researcher_id" value=""/>
+                <div class="form-group">
+                  <label for="pdName">Name</label><br/>
+                  <input type="text" name="name" placeholder="" class="form-control"/>
+                </div>
+                <div class="form-group">
+                  <label for="pdEmail">Email address</label><br/>
+                  <input type="email" name="email" placeholder="" class="form-control"/>
+                </div>
+                <div class="form-group">
+                  <label for="pdD">Designation</label><br/>
+                  <input type="text" name="designation" placeholder="" class="form-control"/>
+                </div>
+                <div class="form-group">
+                  <label for="pdPhoneNumber">Telephone Number</label><br/>
+                  <input type="text" name="phone" placeholder="" class="form-control"/>
+                </div>
+                <div class="form-group">
+                  <label for="pdCoCi">Country of incorporation</label><br/>
+                  <input type="text" name="countryincorporation" placeholder="" class="form-control"/>
+                </div>
+                <div class="form-group">
+                  <label for="pd">IDRC Affiliation (if any)</label><br/>
+                  <input type="text" name="affliation" placeholder="" class="form-control"/>
+                </div>
+                <div class="form-group">
+                  <label for="pdCoR">Country of Residence</label>
+                  <br/>
+                  <input type="text" name="residence" placeholder="" class="form-control"/>
+                </div>
+                <div class="form-group">
+                  <label for="pdCoc">Country of Citizenship</label><br/>
+                  <input type="text" name="citizenship" placeholder="" class="form-control"/>
+                </div>
+                <div class="form-group">
+                  <label for="pdGender">Gender</label>
+                  <br/>
+                  <label class="radio-inline">
+                    <input type="radio" name="gender" id="pdMale" value="male">
+                    Male </label>
+                  <label class="radio-inline">
+                    <input type="radio" name="gender" id="pdFemale" value="female">
+                    Female </label>
+                  <br/>
+                </div>
+                <div class="form-group">
+                  <label for="pdIoRn">Institution or organization name</label><br/>
+                  <input type="text" name="organization" placeholder="" class="form-control"/>
+                </div>
+                <div class="form-group">
+                  <label for="pdOa">Office Address</label><br/>
+                  <textarea name="address" placeholder="" rows="3" class="form-control ckeditor"></textarea>
+                </div>
+              </div>
+              <div class="col-md-6">                                     
+                <div class="form-group">
+                  <label for="pdAoEaI">Areas of Expertise and Interest</label>
+                  <br/>
+                  <textarea type="text" name="expertise" placeholder="" class="form-control ckeditor" rows="3"></textarea>
+                </div>
+                <div class="form-group">
+                  <label for="pdRPoRO">Relevant Publications or Research Outputs</label>
+                  <br/>
+                  <textarea type="text" name="publications" placeholder="" class="form-control ckeditor" rows="3"></textarea>
+                </div>
+                <div class="form-group">
+                  <label for="pd">Website</label>
+                  <br/>
+                  <input type="text" name="website" placeholder="" class="form-control"/>
+                </div>
+                <div class="form-group">
+                  <label for="pdCoI">Country of incorporation</label>
+                  <br/>
+                  <input type="text" name="countryincorporation" placeholder="" class="form-control"/>
+                </div>
+                <div class="form-group">
+                  <label for="pdCoI">Role in Proposed Projec</label>
+                  <br/>
+                  <input type="text" name="role" placeholder="" class="form-control"/>
+                </div>
+              </div>
+              <div class="col-md-12">
+              
+               <button type="submit" class="btn btn-default">Save Edit </button>                                           
+              </div>
+              
+            </fieldset>
+          </form>
+        </div>
+    </div>
+</div>
+<div class="footer">
+      <div class="container">
+        <p class="text-muted">ocsdnet</p>
+      </div>
+</div>
+
+
+  <script src="<?php echo base_url(); ?>public/new/js/jquery-2.0.0.min.js"></script>
+  <script src="<?php echo base_url(); ?>public/new/js/bootstrap.min.js"></script>
+
+  
+
+
+  
+</body>
+</html>
